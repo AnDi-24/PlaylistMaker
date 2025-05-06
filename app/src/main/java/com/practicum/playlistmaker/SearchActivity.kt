eb8da2010e083b45426887bf6bf8d218dfcb1f6d
AnDi24 <setwe@mail.ru>
2025-05-06 14:22:20 +1000

    API commit

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,24 +1,51 @@
 package com.practicum.playlistmaker
 
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
-
-    private var inputValue: String = INPUT_DEF
 
     companion object {
         const val INPUT_STATE = "INPUT_STATE"
         const val INPUT_DEF = ""
     }
+
+    private val iTunesUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(iTunesApi::class.java)
+
+    private var inputValue: String = INPUT_DEF
+
+    private val tracks = ArrayList<Track>()
+
+    private val adapter = TrackAdapter()
+
+    private lateinit var placeholderMessage: TextView
+    private lateinit var placeholderNotFound: ImageView
+    private lateinit var refreshButton: ImageView
+    private lateinit var inputEditText: EditText
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -34,28 +61,39 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val tracks = TrackAdapter(
-            listOf(
-                Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-                Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-                Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-                Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-                Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-            )
-        )
-
-        val rvTrack = findViewById<RecyclerView>(R.id.rvTrack)
-        rvTrack.adapter = tracks
-
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        inputEditText = findViewById<EditText>(R.id.inputEditText)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        placeholderNotFound = findViewById(R.id.placeholderNotFound)
+        refreshButton = findViewById<ImageView>(R.id.refreshButton)
         val backButton = findViewById<ImageView>(R.id.button_back)
         val clearButton = findViewById<ImageView>(R.id.clear_icon)
         val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+        val rvTrack = findViewById<RecyclerView>(R.id.rvTrack)
+
+        adapter.data = tracks
+
+        rvTrack.adapter = adapter
 
         inputEditText.setText(inputValue)
 
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (inputEditText.text.isNotEmpty()) {
+                    requestState()
+                }
+                true
+            }
+            false
+        }
+
+        refreshButton.setOnClickListener {
+            requestState()
+        }
+
         clearButton.setOnClickListener {
             inputEditText.setText("")
+            tracks.clear()
+            adapter.notifyDataSetChanged()
             inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
         }
 
@@ -91,5 +129,53 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    private fun showMessage(text: String, icon: Drawable?, additionalMessage: String) {
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            placeholderNotFound.visibility = View.VISIBLE
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            placeholderMessage.text = text
+            placeholderNotFound.setImageDrawable(icon)
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+                refreshButton.visibility = View.VISIBLE
+                refreshButton.setImageDrawable(getDrawable(R.drawable.refresh_button))
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+            placeholderNotFound.visibility = View.GONE
+            refreshButton.visibility = View.GONE
+        }
+    }
+
+    private fun requestState(){
+        iTunesService.search(inputEditText.text.toString())
+            .enqueue(object : Callback<TracksResponse> {
+                override fun onResponse(call: Call<TracksResponse>,
+                                        response: Response<TracksResponse>) {
+                    if (response.code() == 200) {
+                        tracks.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            tracks.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                        }
+                        if (tracks.isEmpty()) {
+                            showMessage(getString(R.string.nothing_found), getDrawable(R.drawable.nothing), "")
+                        } else {
+                            showMessage("", null, "")
+                        }
+                    } else {
+                        showMessage(getString(R.string.something_went_wrong),getDrawable(R.drawable.went_wrong), response.code().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                    showMessage(getString(R.string.something_went_wrong), getDrawable(R.drawable.went_wrong), t.message.toString())
+                }
+            })
     }
 }
