43a9e3fd30f1c8b0a93490f8f9422c18cfc99503
AnDi24 <setwe@mail.ru>
2025-11-20 14:54:28 +1000

    24th Sprint Commit

--- app/src/main/java/com/practicum/playlistmaker/media/ui/EditPlaylistFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/media/ui/EditPlaylistFragment.kt
@@ -2,7 +2,6 @@ package com.practicum.playlistmaker.media.ui
 
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
